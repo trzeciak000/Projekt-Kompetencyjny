57360006cf179a07f78b88e1d93fcb2a7f412a11
Norbert <norbix30@gmail.com>
2018-12-05 01:51:46 +0100

    Dodane linki do podstron

--- main_panel.php
+++ main_panel.php
@@ -9,13 +9,18 @@
 	</button>
 
     <div class="navbar-collapse collapse" id="navbarToggle" style="">
+        <ul class="navbar-nav m-auto">
+            <li class="nav-item mx-2"><a class="nav-link" href="<?php echo ROOT_PATH; ?>searchsite.php">Znajdź przepis</a></li>
+            <li class="nav item mx-2"><a class="nav-link" href="<?php echo ROOT_PATH; ?>views/listall.php">Wsystkie przepisy</a></li>
+            <li class="nav item mx-2"><a class="nav-link" href="<?php echo ROOT_PATH; ?>views/losowy_przepis.php">Losowe danie</a></li>
+        </ul>
         <ul class="navbar-nav ml-auto">
             <?php if(isset($_SESSION['zalogowany'])) : ?>
                 <li class="nav-item mx-2"><a class="nav-link" href="#">Witaj <?php echo $_SESSION['dane_usera']['login']; ?></a></li>
                 <li class="nav item mx-2"><a class="nav-link" href="<?php echo ROOT_PATH; ?>views/user/logout.php">Wyloguj</a></li>
             <?php else : ?>
-                <li class="nav-item"><a class="nav-link" href="<?php echo ROOT_PATH; ?>views/user/logowanie.php">Zaloguj</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?php echo ROOT_PATH; ?>views/user/rejestracja.php">Zarejestruj</a></li>
+                <li class="nav-item mx-2"><a class="nav-link" href="<?php echo ROOT_PATH; ?>views/user/logowanie.php">Zaloguj</a></li>
+                <li class="nav-item mx-2"><a class="nav-link" href="<?php echo ROOT_PATH; ?>views/user/rejestracja.php">Zarejestruj</a></li>
             <?php endif; ?>
         </ul>
 	</div>
