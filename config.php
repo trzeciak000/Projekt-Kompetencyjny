5b6c6516246f185d24657ef2de8609e2696d1577
Norbert <norbix30@gmail.com>
2019-01-07 22:44:16 +0100

    Dodane _PATH dla css, js, img

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
-  define("DB_HOST", "localhost");
-  define("DB_USER", "root");
-  define("DB_PASS", "");
-  define("DB_NAME", "what2eat");
-  define("ROOT_PATH","/Projekt-Kompetencyjny/");
-  define("ROOT_URL", "http://localhost/Projekt-Kompetencyjny/");
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_NAME", "what2eat");
+    
+    define("ROOT_PATH","/Projekt-Kompetencyjny/");
+    define("ROOT_URL", "http://localhost/Projekt-Kompetencyjny/");
+    define("CSS_PATH", ROOT_PATH . "css/");
+    define("JS_PATH", ROOT_PATH . "js/");
+    define("IMG_PATH", ROOT_PATH . "img/");
 ?>
