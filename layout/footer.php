7f31dc4666efd24bfd2016d73cf37c7877690fab
Norbert <norbix30@gmail.com>
2019-01-09 00:59:34 +0100

    v0.93

--- layout/footer.php
+++ layout/footer.php
@@ -1,26 +1,23 @@
-<footer class="footer-section set-bg" data-setbg="<?php echo IMG_PATH.'footer-bg.jpg' ?>">
+<footer class="footer-section set-bg" style="background-image: url(<?php echo ROOT_URL.'img/footer-bg.jpg' ?>)">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-6">
 				<div class="footer-logo">
-					<img src="<?php echo IMG_PATH.'logo.png' ?>" alt="logo">
+					<img src="<?php echo ROOT_URL.'img/logo.png' ?>" alt="logo">
 				</div>
-				<!--<div class="footer-social">
+				<div class="footer-social">
 					<a href="#"><i class="fa fa-pinterest"></i></a>
 					<a href="#"><i class="fa fa-facebook"></i></a>
 					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-dribbble"></i></a>
-					<a href="#"><i class="fa fa-behance"></i></a>
 					<a href="#"><i class="fa fa-linkedin"></i></a>
-				</div>-->
+				</div>
 			</div>
 			<div class="col-lg-6 text-lg-right">
 				<ul class="footer-menu">
-					<li><a href="#">Główna</a></li>
-					<li><a href="#">Wszystkie</a></li>
-					<li><a href="#">Wyszukaj</a></li>
-					<li><a href="#">Losowy</a></li>
-					<li><a href="#">Lodówka</a></li>
+					<li><a href="<?php echo ROOT_URL; ?>">Główna</a></li>
+					<li><a href="<?php echo ROOT_URL; ?>views/listall.php">Wszyskie</a></li>
+					<li><a href="<?php echo ROOT_URL; ?>views/listall.php">Wyszukaj</a></li>
+					<li><a href="<?php echo ROOT_URL; ?>przepis.php?id=0">Losowy</a></li>
 				</ul>
 				<p class="copyright">
                     &copy;2019 Grupa Pierwsza | Brak praw zastrzeżonych | Ta strona powstała na Projekt Kompetencyjny <i class="fa fa-heart-o" aria-hidden="true"></i>
--- layout/navbar.php
+++ layout/navbar.php
@@ -1,24 +1,22 @@
 <header class="header-section">
 	<div class="header-top">
 		<div class="container">
-			<!--<div class="header-social">
+			<div class="header-social">
 				<a href="#"><i class="fa fa-pinterest"></i></a>
 				<a href="#"><i class="fa fa-facebook"></i></a>
 				<a href="#"><i class="fa fa-twitter"></i></a>
-				<a href="#"><i class="fa fa-dribbble"></i></a>
-				<a href="#"><i class="fa fa-behance"></i></a>
 				<a href="#"><i class="fa fa-linkedin"></i></a>
-            </div>-->
+            </div>
 			<div class="user-panel">
-				<a href="#">Rejestracja</a> / 
-				<a href="#">Logowanie</a>
+				<a href="<?php echo ROOT_URL; ?>views/user/rejestracja.php">Rejestracja</a> / 
+				<a href="<?php echo ROOT_URL; ?>views/user/logowanie.php">Logowanie</a>
 			</div>
 		</div>
 	</div>
 	<div class="header-bottom">
 		<div class="container">
-			<a href="index.html" class="site-logo">
-				<img src="img/logo.png" alt="">
+			<a href="<?php echo ROOT_URL; ?>" class="site-logo">
+				<img src="<?php echo ROOT_URL.'img/logo.png' ?>" alt="">
 			</a>
 			<div class="nav-switch">
 				<i class="fa fa-bars"></i>
@@ -27,11 +25,10 @@
 				<a href="#"><i class="fa fa-search"></i></a>
 			</div>
 			<ul class="main-menu">
-				<li><a href="#">Główna</a></li>
-				<li><a href="#">Wszyskie</a></li>
-				<li><a href="#">Wyszukaj</a></li>
-				<li><a href="#">Losowy</a></li>
-				<li><a href="#">Lodówka</a></li>
+				<li><a href="<?php echo ROOT_URL; ?>">Główna</a></li>
+				<li><a href="<?php echo ROOT_URL; ?>views/listall.php">Wszyskie</a></li>
+				<li><a href="<?php echo ROOT_URL; ?>views/listall.php">Wyszukaj</a></li>
+				<li><a href="<?php echo ROOT_URL; ?>przepis.php?id=0">Losowy</a></li>
 			</ul>
 		</div>
 	</div>
