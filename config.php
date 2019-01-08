96a046e28d833cda8298db2d4e5f51fc52d2be69
Norbert <norbix30@gmail.com>
2019-01-08 01:08:04 +0100

    dod. LAYOUT_PATH

--- config.php
+++ config.php
@@ -4,8 +4,9 @@
     define("DB_PASS", "");
     define("DB_NAME", "what2eat");
     
-    define("ROOT_PATH","/Projekt-Kompetencyjny/");
     define("ROOT_URL", "http://localhost/Projekt-Kompetencyjny/");
+    define("ROOT_PATH", realpath("/Projekt-Kompetencyjny/"));
+    define("LAYOUT_PATH", ROOT_PATH . "layout/");
     define("CSS_PATH", ROOT_PATH . "css/");
     define("JS_PATH", ROOT_PATH . "js/");
     define("IMG_PATH", ROOT_PATH . "img/");
