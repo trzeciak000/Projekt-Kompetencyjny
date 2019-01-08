e9dc0ed6eb65be42e0f22b70a27217596bd177fb
Bogost <bogumilostapiuk@gmail.com>
2019-01-09 00:39:32 +0100

    Lodówka (do obrupki graficznej)

--- search.php
+++ search.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+require "config.php";
+$i = 0;
+if( isset($_SESSION['dane_usera']['id']) )
+{
+	$IDUzytkownika = $_SESSION['dane_usera']['id'];
+	
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	if (!$conn) {
+		die("Connection to server failed: " . mysqli_connect_error());
+	}
+    $conn->query("SET CHARSET utf8");
+	$sql = 
+	"SELECT s.Nazwa, m.Ilosc, m.Jednostka FROM
+		(SELECT * FROM magazyn AS m WHERE IDUzytkownika=$IDUzytkownika ) AS m
+	JOIN skladniki AS s ON s.IDSkladnika=m.IDSkladnika";
+	$wynik = mysqli_query( $conn, $sql );
+?>
 <!doctype html>
 <html lang="pl">
   <head>
@@ -15,30 +34,65 @@
         <div class="container">
           <div class="row" style="align-items: center; justify-content: center;">
             <div class="col-md-4">
-                <form class="centeredform">
+                <form action="proces_search.php" method="post" class="centeredform" >
+<?php
+	while( $rzad = mysqli_fetch_assoc( $wynik ) )
+	{
+?>
                   <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Dodaj produkt" value="Makaron" style="color:black">
+                    <input type="text" class="form-control" 
+						value="<?php echo $rzad['Nazwa'];?>"
+						placeholder="Produkt"
+						style="width: 120px" name="<?php echo "nazwa_" . $i;?>">
+						
                     <div class="input-group-append">
-                      <input type="number" class="input-group" placeholder="Ilość" value="500" style="width: 120px">
+                      <input type="text" pattern="^\d*\.?\d*$"
+						class="input-group" value="<?php echo $rzad['Ilosc'];?>"
+						placeholder="Ilosc"
+						style="width: 60px" name="<?php echo "ilosc_" . $i;?>">
+						
                       <div class="input-group-append">
-                          <div class="input-group-text">g</div>
+                          <input type="text" class="input-group-text"
+						  value="<?php echo $rzad['Jednostka'];?>"
+						  placeholder="J."
+						  style="width: 60px"
+						  name="<?php echo "jednostka_" . $i;?>">
                       </div>
                     </div>
                   </div>
+<?php
+		$i++;
+	}
+}
+?>
+					
                   <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Dodaj produkt" value="Kurczak" style="color:black">
+                    <input type="text" class="form-control" 
+						placeholder="Produkt"
+						style="width: 120px" name="<?php echo "nazwa_" . $i;?>">
+						
                     <div class="input-group-append">
-                        <input type="number" class="input-group" placeholder="Ilość" style="width: 120px">
-                        <div class="input-group-append">
-                            <div class="input-group-text">g</div>
-                        </div>
+                      <input type="text" pattern="^\d*\.?\d*$"
+						class="input-group" placeholder="Ilosc"
+						style="width: 60px" name="<?php echo "ilosc_" . $i;?>">
+						
+                      <div class="input-group-append">
+                          <input type="text" class="input-group-text"
+						  placeholder="J."
+						  style="width: 60px"
+						  name="<?php echo "jednostka_" . $i;?>">
                       </div>
+                    </div>
                   </div>
-                  <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Dodaj produkt">
-                  </div>
-                  
-                  <button class="btn btn-info btn-block"><i class="fas fa-plus"></i></button>
+<?php $i++; ?>
+                  <input id="iloscSkladnikow" type="number" name="ilosc_skladnikow" value="<?php echo $i?>" hidden>
+				  <input type="text" name="id_uzytkownika" value="<?php echo $IDUzytkownika?>" hidden>
+                  <!--<button id="dodajButton" class="btn btn-info btn-block"><i class="fas fa-plus"></i></button>-->
+				  <input type="button" id="dodajButton" value="+">
+				  <input type="submit" value="submit"
+				  <?php if( !isset($_SESSION['dane_usera']['id']) )
+						echo 'disabled';
+					?>>
                 </form>
             </div>
 
@@ -94,5 +148,35 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/custom.js"></script>
     <script src="js/all.min.js"></script>
+	<script>
+$(document).ready(function(){
+	//dodawanie wierszy
+    $("#dodajButton").click(function(){
+		var is = $("#iloscSkladnikow").val();
+        $("#dodajButton").before(`
+			<div class="input-group mb-3">
+				<input type="text" class="form-control" 
+					placeholder="Produkt"
+					style="120px" name="nazwa_${is}">
+					
+				<div class="input-group-append">
+				  <input type="text" pattern="^\\d*\\.?\\d*$"
+					class="input-group" placeholder="Ilosc"
+					style="width: 60px" name="ilosc_${is}">
+					
+				  <div class="input-group-append">
+					  <input type="text" class="input-group-text"
+					  placeholder="J."
+					  style="width: 60px"
+					  name="jednostka_${is}">
+				  </div>
+				</div>
+			</div>
+		`);
+		$("#iloscSkladnikow").val( parseInt(is) + 1);
+    });
+	
+});
+	</script>
   </body>
 </html>
