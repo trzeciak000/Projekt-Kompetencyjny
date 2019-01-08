cdc731b43525f855990c64340a3ed1027f9aa341
Norbert <norbix30@gmail.com>
2019-01-08 21:29:53 +0100

    v0.90

--- views/przepis.php
+++ views/przepis.php
@@ -1,6 +1,6 @@
 <?php
     include "../config.php";
-    include "../navbar.php";
+
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     $conn->query("SET CHARSET utf8");
     $sql = 'SELECT * FROM przepisy WHERE IDPrzepisu = ' . $_GET['id'] . ';';
@@ -14,42 +14,159 @@
 
 <!doctype html>
 <html lang="pl">
-<head>
-    <!-- meta -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>What2Eat</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/custom.css">
-</head>
+<?php include LAYOUT_PATH.'head.php' ?>
+
 <body>
 
-<div class="fullscreen">
-    <div class="col-lg-3 col-sm-6 left">
-        <?php if($zdjecie == null || $zdjecie == "") : ?>
-            <img src="../img/przepisy/brak.png" width="200" height="200">
-        <?php else : ?>
-            <img src="../img/przepisy/<?php echo $zdjecie; ?>" alt="../img/przepisy/brak.png" width="200" height="200">
-        <?php endif; ?>
-    </div>
-    <div class="col-lg-9 col-sm-18 right">
-        <h2>Nazwa:</h2>
-        <?php echo $przepis["Nazwa"]; ?><br /><br />
-        <h4>Opis:</h4>
-        <?php
-        if($przepis["Opis"] == "") echo "brak opisu";
-        else echo $przepis["Opis"];
-        ?><br /><br />
-        <h4>Porcji:</h4>
-        <?php echo $przepis["IloscPorcji"]; ?><br /><br />
-        <h4>Czas przygotowania:</h4>
-        <?php echo $przepis["CzasPrzygotowania"]; ?><br /><br />
-        <h4>Przygotowanie :</h4>
-        <?php echo $przepis["Przygotowanie"]; ?>
-    </div>
+<?php include LAYOUT_PATH.'navbar.php' ?>
+
+<!-- Hero section -->
+<section class="page-top-section set-bg" data-setbg="img/page-top-bg.jpg">
+	<div class="container">
+		<h2><?php echo $przepis['Nazwa']; ?></h2>
+	</div>
+</section>
+<!-- Hero section end -->
+	
+<!-- Recipe big view -->
+<div class="recipe-view-section">
+	<div class="rv-warp set-bg" data-setbg="img/recipes/single-big.jpg"></div>
 </div>
+
+	<!-- Recipe details section -->
+	<section class="recipe-details-section">
+		<div class="container">
+			<div class="recipe-meta">
+				<div class="racipe-cata">
+					<span>For Begginers</span>
+				</div>
+				<h2>Lamb Chops with green peper sauce</h2>
+				<div class="recipe-date">May 15, 2018</div>
+				<div class="rating">
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star"></i>
+					<i class="fa fa-star is-fade"></i>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-5">
+					<div class="recipe-filter-warp">
+						<div class="filter-top">
+							<div class="filter-top-text">
+								<p>Prep: 15 mins</p>
+								<p>Cook: 30 mins</p>
+								<p>Yields: 8 Servings</p>
+							</div>
+						</div>
+						<!-- recipe filter form -->
+						<div class="filter-form-warp">
+							<h2>Ingredients</h2>
+							<form class="filter-form">
+								<div class="check-warp">
+									<input type="checkbox" id="one">
+									<label for="one">4 Tbsp (57 gr) butter</label>
+								</div>
+								<div class="check-warp">
+									<input type="checkbox" id="two">
+									<label for="two">2 large eggs</label>
+								</div>
+								<div class="check-warp">
+									<input type="checkbox" id="three">
+									<label for="three">2 yogurt containers granulated sugar</label>
+								</div>
+								<div class="check-warp">
+									<input type="checkbox" id="four">
+									<label for="four">1 vanilla or plain yogurt, 170g container</label>
+								</div>
+								<div class="check-warp">
+									<input type="checkbox" id="five">
+									<label for="five">White flour</label>
+								</div>
+								<div class="check-warp">
+									<input type="checkbox" id="six">
+									<label for="six">1.5 yogurt containers milk</label>
+								</div>	
+								<div class="check-warp">
+									<input type="checkbox" id="seven">
+									<label for="seven">1/4 tsp cinnamon</label>
+								</div>
+								<div class="check-warp">
+									<input type="checkbox" id="eight">
+									<label for="eight">1 cup fresh blueberries </label>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-7">
+					<ul class="recipe-info-list">
+						<li>
+							<h2>01.</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magni.</p>
+						</li>
+						<li>
+							<h2>02.</h2>
+							<p>Sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus.</p>
+						</li>
+						<li>
+							<h2>03.</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum</p>
+						</li>
+						<li>
+							<h2>04.</h2>
+							<p>Sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis.</p>
+						</li>
+						<li>
+							<h2>05.</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec varius dui. Suspendisse potenti. Vestibulum ac pellentesque tortor. Aenean congue sed metus in iaculis. Cras a tortor enim. Phasellus posuere vestibulum ipsum, eget lobortis purus. Orci varius natoque penatibus et magni.</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Recipe details section end -->
+
+
+	<!-- Comment section -->
+	<section class="comment-section spad pt-0">
+		<div class="container">
+			<h4>Leave a comment</h4>
+			<form class="comment-form">
+				<div class="row">
+					<div class="col-md-6">
+						<input type="text" placeholder="Name">
+					</div>
+					<div class="col-md-6">
+						<input type="text" placeholder="E-mail">
+					</div>
+					<div class="col-md-12">
+						<input type="text" placeholder="Subject">
+						<textarea placeholder="Message"></textarea>
+						<button class="site-btn">Send</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</section>
+	<!-- Comment section -->
+
+
+	<!-- Gallery section -->
+	<div class="gallery">
+		<div class="gallery-slider owl-carousel">
+			<div class="gs-item set-bg" data-setbg="img/instagram/1.jpg"></div>
+			<div class="gs-item set-bg" data-setbg="img/instagram/2.jpg"></div>
+			<div class="gs-item set-bg" data-setbg="img/instagram/3.jpg"></div>
+			<div class="gs-item set-bg" data-setbg="img/instagram/4.jpg"></div>
+			<div class="gs-item set-bg" data-setbg="img/instagram/5.jpg"></div>
+			<div class="gs-item set-bg" data-setbg="img/instagram/6.jpg"></div>
+		</div>
+	</div>
+	<!-- Gallery section end -->
 
 <!-- JavaScript -->
 <script src="../js/jquery-3.3.1.min.js"></script>
