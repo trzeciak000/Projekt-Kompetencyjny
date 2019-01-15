706cab48b287808922c0a07f5436bb0c7a3da546
Norbert <norbix30@gmail.com>
2019-01-15 22:28:59 +0100

    php include navbar

--- views/listall.php
+++ views/listall.php
@@ -7,60 +7,6 @@
 <html>
 <?php include '../layout/head.php' ?>
 <body>
-<<<<<<< HEAD
-<?php include '../layout/navbar.php' ?>
-<!--<form method="post" action="listall.php">-->
-<!--    <label>Zobacz wg kategorii: </label>-->
-<!--    <select name="category">-->
-<!--        <option value="0">all</option>-->
-<!--        --><?php //foreach ($kategorie as $cat) : ?>
-<!--            <option value="--><?php //echo $cat['id']; ?><!--">--><?php //echo $cat['kategoria']; ?><!--</option>-->
-<!--        --><?php //endforeach; ?>
-<!--    </select>-->
-<!--    <input class="btn btn-primary" type="submit" name="kategoria" value="Zobacz">-->
-<!--</form><br />-->
-    <div class="fullscreen" style="overflow-y: scroll;">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-sm-8 col-lg-6">
-                    <h4 class="text-dark">Przepisy</h4>
-                    <form method="post" action="listall.php">
-						<div class="form-row align-items-center">
-                            <div class="col-md-3">
-                                <label class="text-dark">Pokaż </label>
-                            </div>
-                            <div class="col-md-5">
-                                <select name="pokaz" id="pokazSelect" type="text" class="form-control"
-								<?php if( !isset($_SESSION['dane_usera']['id']) )
-										echo 'disabled';
-								?>>
-                                    <option value="0">Wszystkie</option>
-                                    <option value="1">Dające się zrealizować</option>
-                                </select>
-                            </div>
-                            <div class="col-md-4">
-                            </div>
-                        </div>
-                        <div class="form-row align-items-center">
-                            <div class="col-md-3">
-                                <label class="text-dark">Sortuj według </label>
-                            </div>
-                            <div class="col-md-5">
-                                <select name="sortowanie" id="sortSelect" type="text" class="form-control">
-                                    <option value="0">Nazwa A-Z</option>
-                                    <option value="1">Nazwa Z-A</option>
-                                    <option value="2">Czas rosnąco</option>
-                                    <option value="3">Czas malejąco</option>
-                                </select>
-                            </div>
-                            <div class="col-md-4">
-                                <!--<button name="sortuj" type="submit" class="btn btn-outline-dark btn-block">Sortuj</button>-->
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-=======
     <?php include '../layout/navbar.php' ?>
 
 
@@ -72,7 +18,6 @@
 	</section>
 	<!-- Hero section end -->
 
->>>>>>> gui
 
 	<!-- Search section -->
 	<div class="search-form-section">
