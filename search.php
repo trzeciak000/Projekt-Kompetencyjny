0125b9b325f96b21d4d9ff3ed9080157f9fe0658
Bogost <bogumilostapiuk@gmail.com>
2019-01-15 19:54:44 +0100

    Pobieranie jQuery z google (na chwile obecną)

--- search.php
+++ search.php
@@ -143,14 +143,22 @@ if( isset($_SESSION['dane_usera']['id']) )
     </div>
 
     <!-- JavaScript -->
-    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/custom.js"></script>
     <script src="js/all.min.js"></script>
 	<script>
+	window.onload = function() {
+		if (window.jQuery) {  
+		} else {
+			// jQuery is not loaded
+			alert("JQuery niezaladowane");
+		}
+	}
 $(document).ready(function(){
 	//dodawanie wierszy
+	
     $("#dodajButton").click(function(){
 		var is = $("#iloscSkladnikow").val();
         $("#dodajButton").before(`
