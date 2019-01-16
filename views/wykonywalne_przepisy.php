d85d9fb032c24fc3b4fd2fcde287d370dc7f5f6e
Norbert <norbix30@gmail.com>
2019-01-16 01:00:15 +0100

    v1.0

--- views/wykonywalne_przepisy.php
+++ views/wykonywalne_przepisy.php
@@ -50,23 +50,23 @@
 ?>
 <?php foreach ($przepisy as $przepis) : ?>
 <?php
-	$sql = "SELECT Link FROM obrazy_przepisy WHERE IDPrzepisu = '" . $przepis['IDPrzepisu'] . "' LIMIT 1;";
+	$sql = "SELECT Link FROM obrazy_przepisy WHERE IDPrzepisu = ".$przepis['IDPrzepisu']." LIMIT 1;";
 	$zdjecie = $conn->query($sql)->fetch_assoc();
 	$zdjecie = $zdjecie['Link'];
 ?>
-<div class="card">
-	<img class="card-img-top"
-		<?php if ($zdjecie == null || $zdjecie == "") : ?> src="<?php echo ROOT_PATH; ?>img/przepisy/brak.png"
-		<?php else : ?> src="<?php echo ROOT_PATH; ?>img/przepisy/<?php echo $zdjecie; ?>"<?php endif; ?>
-	>    
-	<div class="card-body">
-		<h5 class="card-title"><?php echo $przepis['Nazwa']; ?></h5>
-		<p class="card-text"><?php echo $przepis['Opis'] ?></p>
-		<p class="card-tex">
-			<i class="fas fa-th"></i> <?php foreach($kategorie as $row) {if($row['id'] == $przepis['id_kategorii']){echo $row['kategoria']; break;}}?>
-			<i class="far fa-clock"></i> <?php echo $przepis['CzasPrzygotowania']; ?>
-		</p>
-	</div>
-	<a class="btn btn-info" href="przepis.php?id=<?php echo $przepis['IDPrzepisu']; ?>">PrzejdÅº</a>
+<div class="col-lg-4 col-md-6">
+	<a href="<?php echo ROOT_URL.'views/przepis.php?id='.$przepis['IDPrzepisu']; ?>">
+		<div class="recipe">
+			<div class="recipe-image setbg" style="background-image: url(<?php echo ROOT_URL.'img/przepisy/'.$zdjecie; ?>);"></div>
+			<div class="recipe-info-warp">
+				<div class="recipe-info">
+					<h3><?php echo $przepis['Nazwa']; ?></h3>
+					<div class="rating">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</a>
 </div>
 <?php endforeach; ?>
